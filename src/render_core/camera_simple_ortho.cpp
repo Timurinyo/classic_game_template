f258025b84923fe550a115fb042299d90ba5e81d
shiro <shironecko.mail@gmail.com>
2020-06-05 17:45:32 +0300

    add basic isometric rendering approximation to camera

--- src/render_core/camera_simple_ortho.cpp
+++ src/render_core/camera_simple_ortho.cpp
@@ -9,9 +9,14 @@ glm::mat4 CameraSimpleOrtho::GetViewProjection() const
 {
     const float unitsPerPixel = 1.0f / pixelsPerUnit;
 
-    const glm::vec2 centerOffset = unitsPerPixel * glm::vec2(
+
+    const glm::mat4 isometricRotation = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    const glm::vec3 up = isometricRotation * glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+
+    glm::vec2 centerOffset = unitsPerPixel * glm::vec2(
         (float)(windowWidth / 2),
-        (float)(windowHeight / 2));
+        windowHeight * -1.0f);
+    centerOffset = isometricRotation * glm::vec4(centerOffset, 0.0f, 0.0f);
 
     glm::vec2 alignedPosition = position - centerOffset;
 
@@ -21,13 +26,13 @@ glm::mat4 CameraSimpleOrtho::GetViewProjection() const
     const glm::mat4 view = glm::lookAt(
         glm::vec3(alignedPosition, -1.0f),
         glm::vec3(alignedPosition, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f));
+        up);
 
     const glm::mat4 projection = glm::ortho(
         0.0f,
         windowWidth * unitsPerPixel,
         0.0f,
-        windowHeight * unitsPerPixel,
+        windowHeight * 2.0f * unitsPerPixel,
         0.0f,
         1.0f);
 
