3b4848e15dbce0f0c5b18b8cc15100079c3727ef
vkuksa <volodymyr.kuksa@ubisoft.com>
2020-06-06 18:20:31 +0300

    finished doWhile

--- src/examples/basic/CommandQueue.cpp
+++ src/examples/basic/CommandQueue.cpp
@@ -1,4 +1,5 @@
 #include "CommandQueue.h"
+#include "BasicPlayer.h"
 
 CommandQueue::CommandQueue(int size /*= 5*/)
     : m_Size(size)
@@ -56,30 +57,58 @@ void CommandQueue::Reset()
 }
 
 
-void CommandQueue::StepForward()
+void CommandQueue::StepForward(GameTile::Type previousViewedTile, GameTile::Type currentViewedTile)
 {
     ++m_CommandIndx;
 
     if (m_CommandIndx < m_Commands.size() && m_CommandIndx > 0)
     {
-        while (m_Commands[m_CommandIndx].ID == CommandID::Repeat)
+        while (m_Commands[m_CommandIndx].ID == CommandID::Repeat || m_Commands[m_CommandIndx].ID == CommandID::DoWhile)
         {
             Command& currentCommand = m_Commands[m_CommandIndx];
             if (m_CommandIndx == 0) return;
 
-            if (--currentCommand.CurrentRepeats > 0)
+            switch (currentCommand.ID)
             {
-                --m_CommandIndx;
-                if (m_Commands[m_CommandIndx].ID == CommandID::Repeat)
+                case CommandID::Repeat:
                 {
-                    ++m_Commands[m_CommandIndx].CurrentRepeats;
+                    if (--currentCommand.CurrentRepeats > 0)
+                    {
+                        --m_CommandIndx;
+                        if (m_Commands[m_CommandIndx].ID == CommandID::Repeat)
+                        {
+                            ++m_Commands[m_CommandIndx].CurrentRepeats;
+                        }
+                    }
+                    else
+                    {
+                        ++m_CommandIndx;
+                        currentCommand.Reset();
+                    }
+                    break;
                 }
+                case CommandID::DoWhile:
+                {
+                    if (currentCommand.LastMemorizedTile == GameTile::Type::Undefined)
+                    {
+                        currentCommand.LastMemorizedTile = previousViewedTile;
+                    }
+                    else if (currentViewedTile != GameTile::Type::Goal && currentViewedTile != currentCommand.LastMemorizedTile)
+                    {
+                        ++m_CommandIndx;
+                        currentCommand.Reset();
+                    }
+                    else
+                    {
+                        --m_CommandIndx;
+                    }
+                    break;
+                }
+                default:
+                    break;
             }
-            else
-            {
-                ++m_CommandIndx;
-                currentCommand.Reset();
-            }
+
+            
         }
     }
 }
